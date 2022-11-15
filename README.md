f14cd398a5f85d6f43eae8b0cdbb9be49eb469a8
Alexander <sashafromlibertalia@gmail.com>
2022-11-15 22:21:34 +0300

    feat: added basic commands

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Автор плагина - Александр Мирошниченко, М33011
 
-This is the README for your extension "devtools-extension". After writing up a brief description, we recommend including the following sections.
-
 ## Описание плагина
+
+Данный плагин предназначен для скрытия файлов и папок, прописанных в `.gitignore`.
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fs = require('fs');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const [folder] = [...vscode.workspace.workspaceFolders ?? []];
+const folderPath = folder.uri.path;
+
+const data = fs.readFileSync(`${folderPath}/.gitignore`, 'utf8');
+const files = data.split('\n').filter((f: string) => f && !f.includes('#'));
+
 export function activate(context: vscode.ExtensionContext) {
+	const hideCommand = 'devtools-extension.hideIgnored';
+	const hideCommandHandler = () => {
+		const config = vscode.workspace.getConfiguration();
+		config.update('files.exclude', files.reduce((acc: any, file) => {
+			acc[file] = true;
+			return acc;
+		}, {}), false);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "devtools-extension" is now active!');
+		vscode.window.showWarningMessage(`Hiding ${files.length} files`);
+	};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('devtools-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from devtools-extension!');
-	});
+	const showCommand = 'devtools-extension.showIgnored';
+	const showCommandHandler = () => {
+		const config = vscode.workspace.getConfiguration();
+		config.update('files.exclude', files.reduce((acc: any, file) => {
+			acc[file] = false;
+			return acc;
+		}, {}), false);
+		vscode.window.showWarningMessage(`Showing ${files.length} files`);
+	};
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand(hideCommand, hideCommandHandler));
+	context.subscriptions.push(vscode.commands.registerCommand(showCommand, showCommandHandler));
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
