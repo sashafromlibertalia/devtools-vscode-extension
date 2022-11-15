197434f729ece3e95db8f0319a19f53fa7c64eb3
Alexander <sashafromlibertalia@gmail.com>
2022-11-15 22:56:30 +0300

    refactor: added more vscode API methods

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,45 @@
 import * as vscode from 'vscode';
 import fs = require('fs');
 
-const [folder] = [...vscode.workspace.workspaceFolders ?? []];
-const folderPath = folder.uri.path;
-
-const data = fs.readFileSync(`${folderPath}/.gitignore`, 'utf8');
-const files = data.split('\n').filter((f: string) => f && !f.includes('#'));
-
-export function activate(context: vscode.ExtensionContext) {
-	if (!files.length) {
+export async function activate(context: vscode.ExtensionContext) {
+	const gitignores = await vscode.workspace.findFiles('**/.gitignore');
+	if (!gitignores.length) {
 		vscode.window.showInformationMessage('No files to ignore');
 		return;
 	}
 
+	const files = new Set();
+
+	const gitignorePaths = await Promise.all(gitignores.map(async (file) => {
+		return (await vscode.workspace.openTextDocument(file.path)).fileName;
+	}));
+
+	gitignorePaths.forEach((path) => {
+		const data = fs.readFileSync(path, 'utf8');
+		const filesToIgnore = data.split('\n').filter((f: string) => f && !f.includes('#'));
+		filesToIgnore.forEach(item => files.add(item));
+	});
+
 	const hideCommand = 'devtools-extension.hideIgnored';
 	const hideCommandHandler = () => {
 		const config = vscode.workspace.getConfiguration();
-		config.update('files.exclude', files.reduce((acc: any, file) => {
-			acc[file] = true;
+		config.update('files.exclude', [...files].reduce((acc: any, file) => {
+			acc[file as string] = true;
 			return acc;
 		}, {}), false);
 
-		vscode.window.showWarningMessage(`Hiding ${files.length} files`);
+		vscode.window.showWarningMessage(`Hiding ${[...files].length} files`);
 	};
 
 	const showCommand = 'devtools-extension.showIgnored';
 	const showCommandHandler = () => {
 		const config = vscode.workspace.getConfiguration();
-		config.update('files.exclude', files.reduce((acc: any, file) => {
-			acc[file] = false;
+		config.update('files.exclude', [...files].reduce((acc: any, file) => {
+			acc[file as string] = false;
 			return acc;
 		}, {}), false);
 
-		vscode.window.showWarningMessage(`Showing ${files.length} files`);
+		vscode.window.showWarningMessage(`Showing ${[...files].length} files`);
 	};
 
 	context.subscriptions.push(vscode.commands.registerCommand(hideCommand, hideCommandHandler));
